dd28d7b2ca820aeb9c920d09b11dfed1514eb0ad
Attila Sedon <attila.sedon@sedonattila.com>
2023-08-28 21:38:52 +0200

    first steps

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import(
 	"fmt"
+	"bufio"
+	"os"
 )
 
+
 func main() {
-	fmt.Println("Hello, pokedex")
+	for {
+		fmt.Printf("pokedex > ")
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n');
+		if(err != nil) {
+			fmt.Printf("The following error happened:")
+			fmt.Printf(err.Error())
+			break
+		}else if(text == "exit") {
+			break
+		}else if(text == "help") {
+			fmt.Println()
+		} else {
+		  fmt.Println(text);
+		}
+		
+	}
 }
 
